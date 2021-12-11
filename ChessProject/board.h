ce36488822feb765d21a120ee9a4c8869d0d6342
Rico9939 <dinosaurgaming35@gmail.com>
2021-12-11 14:24:46 +0200

    feat: added: constructor, distructor, print board
    i didnt add the check for check beacause we need first of all to check the pawn and then the check . last , didnt add the get board string because we have it in the constructor

--- ChessProject/board.h
+++ ChessProject/board.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "stdafx.h"
+#include <iostream>
+class Board
+{
+public:
+	Piece** board;//board[][] doesnt work
+	bool* arrayChess;
+	bool* arrayTurn;
+	Board(const std::string& startingBoard);
+	~Board();
+	//std::string getBoardString(); didnt understand why we need get when we initilize it on the constructor
+	void printBoard() const;
+	//check for check didnt done
+};
