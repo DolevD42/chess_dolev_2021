4e73b4f1358bd0e1df29b79a7fa5dea554fbc598
Rico9939 <dinosaurgaming35@gmail.com>
2021-12-11 14:08:09 +0200

    feats: added: piece con, move piece, check for valid move
    this add was in the edge of some rules i didnt understand so i added this and its other edit

--- ChessProject/piece.h
+++ ChessProject/piece.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "board.h"
+#include "stdafx.h"
+
+
+class Piece
+{
+protected:
+	int color; //0  white 1 black  #insted of using the  color color that i dont have
+	int xAxis;
+	int yAxis;
+	//char symbol; // can be used if we want insted of type()
+public:
+	Piece(const int x, const int y, const int color);
+	~Piece();
+	virtual std::string movePiece(std::string STmovDS, Board& board) = 0;//we can use this function as int not string because we return the int of the error and not ther error itself
+	virtual std::string* CheckMovesValidation(Board& board) = 0;//didnt understand why we return bool how can we understand what kind of error is this
+};
